6375677981e431efb11bc3ccbb88f2a56327427f
Ghsjulian <ghsjulian@gmail.com>
2024-06-17 12:50:49 +0600

    Success

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -19,6 +19,12 @@ const AboutSection = () => {
                             am doing a graduation in English literature. I live
                             with my parents in a small village of Bangladesh. My
                             biggest dream is being a best computer programmer.
+                            I am a simple boy . And i have a Lot's of dream. I
+                            wanna fulfill them and that's why i am busy to make
+                            myself perfect. when i will be a programmer,,, I'll
+                            know that my dream has succeed. I like coding so
+                            much . I am 21 Years old. For more information you
+                            can contact me or follow me on my social media.
                         </article>
                     </div>
                 </div>
